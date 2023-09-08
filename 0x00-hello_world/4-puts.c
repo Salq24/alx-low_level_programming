98eb2e0a4fe8a0a991f1810c6fd90376005edeb5
queen <qogheneochuko@gmail.com>
2023-09-08 14:11:41 -0700

    now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+*main is the entry point of the program
+*stdio.h is an header file. This is an essential
+*part of c programming
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
