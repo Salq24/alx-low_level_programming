805173feed6246578bff745a1aa7e6c739ccb9a1
queen <qogheneochuko@gmail.com>
2023-09-08 14:47:32 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main : entry point
+*
+* description : "with proper grammar, but the outcome is a piece of art,", followed by a new line.
+
+* return : always 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
