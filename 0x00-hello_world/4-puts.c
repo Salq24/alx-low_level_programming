62a01fbb1adb72697a5ac65569255203b1ed6423
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-08 22:18:31 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
-*main is the entry point of the program
-*stdio.h is an header file. This is an essential
-*part of c programming
+*main - the entry point of the program
+*
+*stdio.h is an header file. This is an essential part of c programming
+*
 *Description - print "Programming is like building a multilingual puzzle
+*
 *return -should always be 0
 */
 int main(void)
