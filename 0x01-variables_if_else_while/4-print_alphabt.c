3f5da7734c1390f112abcc6536b8db3bffabcd71
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-09 21:12:54 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
 * main : entry point
-* Description : This project deals with writing alphabeth
-* in lowercase. Only putchar is allowed here.
+* Description : This project deals with writing alphabet
+* except e and q. Only putchar is allowed here.
 * You can use a for loop here or a while loop
 * I will use a while loop in this project.
 *return: always 0 (success)
@@ -14,9 +14,10 @@ char alphabet = 'a';
 
 while (alphabet <= 'z')
 {	if(alphabet != 'e' && alphabet != 'q')
+        {
 		putchar(alphabet);
 	}
-alphabet++;
+        alphabet++;
 }
 putchar('\n')
 return (0);
