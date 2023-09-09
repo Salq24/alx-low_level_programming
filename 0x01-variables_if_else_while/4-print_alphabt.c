79603890a0df7406a0fb7920a8eb0a03fbce708b
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-09 21:17:21 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,12 +13,12 @@ int main(void)
 char alphabet = 'a';
 
 while (alphabet <= 'z')
-{	
+{
 	if (alphabet != 'e' && alphabet != 'q')
 {
 		putchar(alphabet);
 }
-        alphabet++;
+alphabet++;
 }
 putchar('\n');
 return (0);
