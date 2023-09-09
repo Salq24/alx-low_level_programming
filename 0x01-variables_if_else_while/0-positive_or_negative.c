4161237713054f236b422c0a0f2aed9335edd98a
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-09 18:17:30 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
 * main - entry point
-* discription - print whether the number stored in the                                    
+* description - print whether the number stored in the                                    
 * variable n is positive or negative.
 * return - always 0(success)
 */
@@ -16,11 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0) {
-		printf("%d is positive");
+		printf("%d is positive\n");
 	} else if (n == 0) {
-		printf("%d is zero);
-	} else 
-		printf("%d is negative");
-
+		printf("%d is zero\n");
+	} else { 
+		printf("%d is negative\n");
+	}
 	return (0);
 }
