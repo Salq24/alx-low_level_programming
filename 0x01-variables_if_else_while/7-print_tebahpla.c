29210da20c930bfa8af60ffb236711e645f11d80
queen <qogheneochuko@gmail.com>
2023-09-10 05:02:28 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* Main: Print alphabet in reverse, followed by a new line
+*
+*Description : This project deals with printing alphabet in reverse,
+* i.e, starting from z to a in lower case. Only putchar
+* can be used for this. It is a project requirement. Printf
+* works as well tho.
+*
+* Return : Always 0, shows success.
+*
+*/
+
+int main(void)
+{
+char alphabet = 'z';
+
+while (alphabet >= 'a')
+{
+putchar(alphabet);
+alphabet--;
+}
+
+putchar('\n');
+
+return (0);
+}
