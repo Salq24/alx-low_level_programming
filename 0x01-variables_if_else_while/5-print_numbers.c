f8115177c8bed7ef3e0b239fbc5c2f63f450b6e5
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-10 11:44:56 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
 * main: Entry point
 * description: This project requires us to print all
 * single digit numbers, i.e , numbers less than 10,
-* including zero. This can be done using for and while 
+* including zero. This can be done using for and while
 * loops. You can also use printf or putchar to carry
 * it out. ASCII coding is required when you are using
 * putchar because an interger is used in this case.
@@ -18,7 +18,7 @@ int main(void)
 {
 	int n = 48;
 
-	while(n < 58)
+	while (n < 58)
 {
 	putchar(n);
 	n++;
