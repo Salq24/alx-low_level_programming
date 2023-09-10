229aeac0cebf88775710b584bc96ba3e7cf551ff
queen <qogheneochuko@gmail.com>
2023-09-10 05:37:31 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*Main : Print all numbers of base 16 in lower case.
+*
+*Description : This project deals with printing all numbers
+*of base 16 in lowercase. To get base 16 numbers, use %x.
+*%X prints them in uppercase letters. Putchar is also a 
+*requirement here and we can also use printf.
+*
+*Return: Always 0 , meaning success
+*/
+
+int main(void)
+{
+int n = 0;
+
+while (n < 48)
+{
+	if (n < 10)
+	putchar(n + '0');
+	else if (n > 41)
+	putchar(n -10 + 'a');
+	n++;
+}
+putchar(10);
+
+return (0);
+}
