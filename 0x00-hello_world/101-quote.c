006255c62f8f1eb3dfbfe4d4b8a24f3d62499751
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 16:48:53 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,19 @@
 #include <unistd.h>
 
 /**
-main:This project wants me to print something, followed
-by a new line to the standard error.
-
-description: Write a C program that prints exactly and
-that piece of art is useful" - Dora Korpar, 2015-10-19,
-followed by a new line, to the standard error.
-
-return: always 0 (success).
+*main:Entry point
+*description: Write a C program that prints exactly and
+*that piece of art is useful" - Dora Korpar, 2015-10-19,
+*followed by a new line, to the standard error.
+*
+*return: always 0 (success).
 */
 
 int main(void)
 /**
-You are not allowed to use any functions listed in the NAME
-section of the man (3) printf or man (3) puts
-Your program should return 1
+*main:You are not allowed to use any functions listed in the NAME
+*section of the man (3) printf or man (3) puts
+*Your program should return 1
 */
 
 {
