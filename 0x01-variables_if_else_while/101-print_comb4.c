9654806f181a030fd7c397f41dfcc7c3822aee7d
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 12:28:50 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,22 +2,19 @@
 
 /**
 * main: Write a program that prints all possible different
-* combinations of two digits.
+* combinations of three digits.
 *
 * description:
 * Numbers must be separated by ,, followed by a space
-* The two digits must be different
-* 01 and 10 are considered the same combination
-* Print only the smallest combination of two digits
-* Numbers should be printed in ascending order, with two digits
+* The three digits must be different
+* Print only the smallest combination of the digits
+* Numbers should be printed in ascending order, with three digits
 * You can only use the putchar function (every other function
 * (printf, puts, etc…) is forbidden)
-* You can only use putchar five times maximum in your code
+* You can only use putchar six times maximum in your code
 * You are not allowed to use any variable of type char
 * All your code should be in the main function
-* This project requires us to print all
-* single digit numbers, i.e , numbers less than 10,
-* including zero. This can be done using for and while
+* This can be done using for and while
 * loops. You can also use printf or putchar to carry
 * it out. ASCII coding is required when you are using
 * putchar because an interger is used in this case.
@@ -31,9 +28,9 @@ int main(void)
 {
 	int dig1, dig2, dig3;
 	
-	for (dig1 = 0; dig1 < 9; dig1++)
+	for (dig1 = 0; dig1 < 8; dig1++)
 {
-	for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+	for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 {
 	for (dig3 = dig2 +1; dig3 < 10; dig3++)
 {
@@ -44,7 +41,7 @@ int main(void)
 	if (dig1 == 7 && dig2 == 8 && dig3 == 9)
 	continue;
 
-	putchat(',');
+	putchar(',');
 	putchar(' ');
 }
 }
