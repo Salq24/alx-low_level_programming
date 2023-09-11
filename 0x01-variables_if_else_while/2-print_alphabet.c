604724d747b1da8502b6568ab77ff42093046f3d
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:46:32 +0100

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-* main : entry point
-* Description : This project deals with writing alphabeth
+*main - entry point
+*
+*Description - This project deals with writing alphabeth
 * in lowercase. Only putchar is allowed here.
 * You can use a for loop here or a while loop
 * I will use a while loop in this project.
-*return: always 0 (success)
+*
+*Return: always 0 (success)
 */
 int main(void)
 {
