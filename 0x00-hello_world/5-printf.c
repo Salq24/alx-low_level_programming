6930d1c2edd4ec05b78d7327c39db305091839cc
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:40:01 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,7 @@
 *Description - "with proper grammar, but the outcome is a piece of art,",
 * followed by a new line.
 *
-*Return : Always 0 (success)
+*Return: Always 0 (success)
 */
 int main(void)
 {
