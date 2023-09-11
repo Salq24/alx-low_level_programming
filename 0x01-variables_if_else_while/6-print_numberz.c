0703b8feca2e816fcac96d86a02180851cb7d181
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:49:39 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
-* main: Print all single digit numbers. The numbers
-* must be less than 10 and 0 is included.
+*main - Print all single digit numbers.
 *
-* description: This project requires us to print all
+* Description - This project requires us to print all
 * single digit numbers, i.e , numbers less than 10,
 * including zero. This can be done using for and while
 * loops. You can also use printf or putchar to carry
@@ -13,7 +12,7 @@
 *The ASCII coding starts from 048 (which is 0). Then
 * 049 (1) and on and on. Also includes alphabets.
 *
-*return : This is always 0 which means success
+*Return: This is always 0 which means success
 */
 
 int main(void)
