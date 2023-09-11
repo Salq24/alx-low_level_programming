2330c560bafee0c447e9455710ad2363bf84c866
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 12:30:04 +0100

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -27,12 +27,12 @@
 int main(void)
 {
 	int dig1, dig2, dig3;
-	
+
 	for (dig1 = 0; dig1 < 8; dig1++)
 {
 	for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 {
-	for (dig3 = dig2 +1; dig3 < 10; dig3++)
+	for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 {
 	putchar((dig1 % 10) + '0');
 	putchar((dig2 % 10) + '0');
