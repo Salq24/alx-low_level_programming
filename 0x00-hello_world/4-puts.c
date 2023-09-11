73f862337dd871456170d1a469840c2011d8e420
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:06:09 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main : Prints "Programming is like building a multilingual
- *                puzzle.
+ * main :"Programming is like building a multilingual puzzle.
  *
  * Return: Always 0.
  */
