e95d614b7e0641ab8bb9112778d386e4cb3c9d4f
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 11:16:24 +0100

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 /**
-* main: Print all single digit numbers. The numbers
-* must be less than 10 and 0 is included.
+* main: Write a program that prints all possible different
+* combinations of two digits.
 *
-* description: This project requires us to print all
+* description:
+* Numbers must be separated by ,, followed by a space
+* The two digits must be different
+* 01 and 10 are considered the same combination
+* Print only the smallest combination of two digits
+* Numbers should be printed in ascending order, with two digits
+* You can only use the putchar function (every other function
+* (printf, puts, etc…) is forbidden)
+* You can only use putchar five times maximum in your code
+* You are not allowed to use any variable of type char
+* All your code should be in the main function
+* This project requires us to print all
 * single digit numbers, i.e , numbers less than 10,
 * including zero. This can be done using for and while
 * loops. You can also use printf or putchar to carry
@@ -20,18 +31,18 @@ int main(void)
 {
 	int dig1,dig2;
 	for (dig1 = 0; dig1 < 9; dig1++)
-	{
-		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
-		{
-			putchar((dig1 % 10) + '0');
-			putchar((dig2 % 10) + '0');
+{
+	for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+{
+	putchar((dig1 % 10) + '0');
+	putchar((dig2 % 10) + '0');
 
-			if (dig1 == 8 && dig2 == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	if (dig1 == 8 && dig2 == 9)
+	continue;
+	putchar(',');
+	putchar(' ');
+}
+}
 	putchar('\n');
 	return (0);
 }
