635f244f69fad2e73a475eb2c198b420bfe2d968
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:42:24 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,15 @@
 #include <unistd.h>
 
 /**
-*main:Entry point
-*description: Write a C program that prints exactly and
+*main - Entry point
+*Description - Write a C program that prints exactly and
 *that piece of art is useful" - Dora Korpar, 2015-10-19,
 *followed by a new line, to the standard error.
 *
-*return: always 0 (success).
+*Return: always 0 (success).
 */
 
 int main(void)
-/**
-*main:You are not allowed to use any functions listed in the NAME
-*section of the man (3) printf or man (3) puts
-*Your program should return 1
-*/
 
 {
 	write(2,
