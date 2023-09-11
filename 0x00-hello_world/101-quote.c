70620b8d8c8258af8e4ea85e276ced9f79be3078
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 16:09:49 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
 * main: Entry point
 *
-* description: Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+* description: Write a C program that prints exactly and
+* that piece of art is useful" - Dora Korpar, 2015-10-19,
+* followed by a new line, to the standard error.
 *
 *return: always 0 (success).
-
 */
 
 int main(void)
