d072f2befa5223d91556d4b6ee62acdb42d2e26e
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 17:47:55 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-* main : entry point
-* Description : This project deals with writing alphabet
+*main - entry point
+*
+*Description - This project deals with writing alphabet
 * except e and q. Only putchar is allowed here.
 * You can use a for loop here or a while loop
 * I will use a while loop in this project.
-*return: always 0 (success)
+*
+*Return: always 0 (success)
 */
 int main(void)
 
