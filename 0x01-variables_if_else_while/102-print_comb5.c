cde887dc5335b87a4f09ed8cb2c059c89b7f8d2d
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-11 13:50:52 +0100

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -28,9 +28,9 @@ int main(void)
 {
 	int dig1, dig2;
 	
-	for (for dig1 = 0; dig1 < 98; dig1++)
+	for (for dig1 = 0; dig1 < 99; dig1++)
 {
-	for (dig2 = dig1 + 1; dig2 < 99; dig2++)
+	for (dig2 = dig1 + 1; dig2 < 100; dig2++)
 {
 	putchar((dig1 / 10) + '0');
 	putchar((dig1 % 10) + '0');
