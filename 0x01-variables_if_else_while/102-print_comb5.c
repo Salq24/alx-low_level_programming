0416f97a91843c3844129729d2252caa87c8f52e
queen <qogheneochuko@gmail.com>
2023-09-11 05:47:26 -0700

    while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+* main: Write a program that prints all possible different
+* combinations of three digits.
+*
+* description:
+* Numbers must be separated by ,, followed by a space
+* The three digits must be different
+* Print only the smallest combination of the digits
+* Numbers should be printed in ascending order, with three digits
+* You can only use the putchar function (every other function
+* (printf, puts, etc…) is forbidden)
+* You can only use putchar six times maximum in your code
+* You are not allowed to use any variable of type char
+* All your code should be in the main function
+* This can be done using for and while
+* loops. You can also use printf or putchar to carry
+* it out. ASCII coding is required when you are using
+* putchar because an interger is used in this case.
+*The ASCII coding starts from 048 (which is 0). Then
+* 049 (1) and on and on. Also includes alphabets.
+*
+*return : This is always 0 which means success
+*/
+
+int main(void)
+{
+	int dig1, dig2;
+	
+	for (for dig1 = 0; dig1 < 98; dig1++)
+{
+	for (dig2 = dig1 + 1; dig2 < 99; dig2++)
+{
+	putchar((dig1 / 10) + '0');
+	putchar((dig1 % 10) + '0');
+	putchar(' ');
+	putchar((dig2 / 10) + '0');
+	putchar((dig2 % 10) + '0');
+
+	if (dig1 == 98 && dig2 == 99)
+	continue;
+
+	putchar(',');
+	putchar(' ');
+}
+}
+	putchar('\n');
+
+	return (0);
+}
