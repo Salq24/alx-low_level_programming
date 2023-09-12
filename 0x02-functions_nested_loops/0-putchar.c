720789dfd066441a5c01a09caa29486bfc3c5168
Salq24 <142992679+Salq24@users.noreply.github.com>
2023-09-12 22:41:53 +0100

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include <main.h>
 /**
-*main - Write a program that prints _putchar, followed by a new line.
-
+*main - Write a program that prints _putchar,
+*followed by a new line.
+*
 *Return: Always 0
 */
 
