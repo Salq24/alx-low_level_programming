8d60606e40889a1ce225251a1daa352f0956c6ec
queen <qogheneochuko@gmail.com>
2023-10-04 00:01:36 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor 
+ * of the number 612852475143
+ *
+ * Return: number
+ */
+
+int main(void)
+{
+	long a, larpf;
+	long number = 612852475143;
+	double square = sqrt(number);
+
+	for (a = 1; a <= square; a++)
+	{
+		if (number % a == 0)
+		{
+		larpf = number / a;
+		}
+	}
+		printf("%ld\n", larpf);
+		return (0);
+}
