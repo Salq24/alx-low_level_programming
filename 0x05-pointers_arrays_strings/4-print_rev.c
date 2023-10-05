d5329b605dd0f78e5c1f5e88ea721779d1384efc
queen <qogheneochuko@gmail.com>
2023-10-05 13:43:18 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,11 +13,11 @@ void print_rev(char *s)
 {
 	int a = 0;
 
-	while(s[a] != '\0')
+	while (s[a] != '\0')
 	{
 	a++;
 	}
-	for (a = a -1; a >= 0; a--)
+	for (a = a - 1; a >= 0; a--)
 	{
 	_putchar(s[a]);
 	}
