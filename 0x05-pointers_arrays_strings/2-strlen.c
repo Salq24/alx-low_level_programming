84773372e9496c026286dbdfddb9f3459968999c
queen <qogheneochuko@gmail.com>
2023-10-05 11:40:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of
+ * a string
+ *
+ * @s - string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s != '\0'; s++)
+
+	{
+		a++;
+	}
+	return (a);
+}
+
