77b259bc975e7fe1a03ccb9be095acab911dc365
queen <qogheneochuko@gmail.com>
2023-10-05 14:02:15 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string
+ *
+ * Return: Void
+ */
+void rev_string(char *s)
+
+{
+	int a, b, c;
+	char *d, ref;
+
+	d = s;
+
+	while (s[b] != '\0')
+	{
+		b++
+	}
+	for (c = 1; c < b; c++)
+	{
+		d++;
+	}
+	for (a = 0; a < (b / 2); a++)
+	{
+		ref = s[i];
+		s[i] = *d;
+		*d = ref;
+		d--;
+	}
+}
+
