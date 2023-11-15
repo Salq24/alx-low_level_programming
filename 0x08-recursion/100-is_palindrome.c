0af261d47abe54e33ec703c881e0cba86c3624ae
Salq24 <qogheneochuko@gmail.com>
2023-11-15 11:23:25 -0800

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - returns string length
+ * @s: string
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen(s + 1));
+}
+
+/**
+ * checks - checks for is_palidrone
+ * @s: string
+ * @len: string length
+ * @cnt: string count
+ * Return: 1 if true, else, 0
+ */
+
+int checks(char *s, int len, int cnt)
+{
+	if (cnt >= len)
+		return (1);
+	if (str[cnt] == str[len])
+		return (checks(str, len - 1, cnt + 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if the words of a string
+ * reads the same backwards or forward
+ *
+ * @s: string
+ * Return: 1 if true, else, 0
+ */
+
+int is_palindrome(char *s)
+
+{
+	int len = _strlen(s);
+	int cnt = 0;
+
+	return (checks(s, len - 1, cnt));
+}
+
