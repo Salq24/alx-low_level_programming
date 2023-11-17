c3c9c41eaa6ffea5036d4f5c15091331fafe07a4
Salq24 <qogheneochuko@gmail.com>
2023-11-17 14:35:00 -0800

    0-whatsmyname

---
+++ 0x0A_argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its own name,
+ * followed by a new line
+ *
+ * @argv: array
+ * @argc: arg count
+ * Return: its own name
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	Return (0);
+}
+
