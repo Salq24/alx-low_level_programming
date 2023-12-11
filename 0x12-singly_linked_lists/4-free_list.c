db40ee7621d2568330f330b5dabdcb9014a8b136
Salq24 <qogheneochuko@gmail.com>
2023-12-11 12:06:35 -0800

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_lust - frees a list_t list
+ * @head: list to be fred
+ */
+
+void free_list(list_t *head)
+{
+	list_t *x;
+
+	while (head)
+	{
+		x = head->next;
+		free(head->str);
+		free(head);
+		head = x;
+	}
+}
