72da4e51b9003c1047a8429e79903844b67739dc
Salq24 <qogheneochuko@gmail.com>
2023-12-13 11:43:28 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * printl - prints before the main
+ * function is executed
+ */
+
+void printl(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
