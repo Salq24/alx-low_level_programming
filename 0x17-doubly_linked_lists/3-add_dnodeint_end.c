5e358f1e401474ef3c0d84af51439828f99b834e
Salq24 <qogheneochuko@gmail.com>
2024-01-26 09:27:40 -0800

    c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *end;
 	dlistint_t *new;
-	
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,7 +8,7 @@
  * Return: node, else, NULL
  */
 
- dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int cnt = 0;
 
