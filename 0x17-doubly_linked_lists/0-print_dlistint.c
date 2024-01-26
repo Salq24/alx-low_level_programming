0c30e76f20bf7504f40e1e68839b2b59e80b3d76
Salq24 <qogheneochuko@gmail.com>
2024-01-26 07:55:35 -0800

    Doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,17 @@
  * Return: num of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h) 
+size_t print_dlistint(const dlistint_t *h)
 {
-    size_t node_cnt = 0;
+	size_t node_cnt = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_cnt++;
+	}
 
-    while (h != NULL) {
-        printf("%d\n", h->n);
-        h = h->next;
-        node_cnt++;
-    }
-
-    return node_cnt;
+	return (node_cnt);
 }
 
