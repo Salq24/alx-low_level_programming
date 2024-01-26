3c7bbded219bf4ec5e91278abada9180ebb5f0c7
Salq24 <qogheneochuko@gmail.com>
2024-01-26 09:06:17 -0800

    free

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node to the end
+ * of dlistint_t list
+ * @head: pointer
+ * @n: data
+ * Return: new elem address
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *end;
+	dlistint_t *new;
+	
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	end = *head;
+
+	while (end->next != NULL)
+	{
+		end = end->next;
+	}
+	new->prev = end;
+	end->next = new;
+
+	return (new);
+}
---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a doubly linked
+ * list dlistint_t
+ * @head: pointer
+ * Return:..
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tp;
+
+	if (head != NULL)
+	{
+		while (head ->prev != NULL)
+			head = head->prev;
+	}
+
+	while ((tp = head) != NULL)
+	{
+		head = head->next;
+		free(tp);
+	}
+}
