261409182a49518e3026670f39eeb4bf560230ff
Salq24 <qogheneochuko@gmail.com>
2024-05-20 03:36:14 -0700

    0-linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x = 0;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array [%li] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
+}
